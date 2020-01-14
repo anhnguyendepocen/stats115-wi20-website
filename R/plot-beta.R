80947948e7dee394951790f4db17ae0f7ec96ed0
mdogucu <mdogucu@gmail.com>
2020-01-14 12:23:42 -0800

    add title to plot-beta.R

--- R/plot-beta.R
+++ R/plot-beta.R
@@ -10,6 +10,8 @@ plot_beta <- function(alpha, beta){
                   args = list(shape1 = alpha,
                               shape2=beta)) +
     labs(x = expression(pi), y =
-           expression(paste("f(",pi,")")))
+           expression(paste("f(",pi,")")),
+         title = paste0("Beta(", alpha, ",",beta, ")")) +
+    theme(plot.title = element_text(hjust = 0.5))
+  
 }
-
